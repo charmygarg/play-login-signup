92a7e29f5f9aa21c3d03bca039dcb705d2a623b4
Charmy Garg <charmiigarg@gmail.com>
2017-03-07 02:48:06 +0530

    Play Assignment

--- app/ErrorHandler.scala
+++ app/ErrorHandler.scala
@@ -8,27 +8,18 @@ class ErrorHandler extends HttpErrorHandler {
   def onClientError(request: RequestHeader, statusCode: Int, message: String) = {
 
     statusCode match {
-      case 400 => Future.successful(
-        Status(statusCode)("Bad Request")
-      )
-      case 401 => Future.successful(
-        Status(statusCode)("Unauthorized")
-      )
-      case 403 => Future.successful(
-        Status(statusCode)("Forbidden area")
-      )
-      case 404 => Future.successful(
-        Status(statusCode)("Page not found")
-      )
-      case 407 => Future.successful(
-        Status(statusCode)("Proxy Authentication Required")
-      )
-      case 500 => Future.successful(
-        Status(statusCode)("Internal Server Error")
-      )
-      case _=> Future.successful (
-        Status (statusCode) ("Something went wrong!!")
-      )
+      case 400 => Future.successful(Status(statusCode)("Bad Request"))
+      case 401 => Future.successful(Status(statusCode)("Authorization Required"))
+      case 403 => Future.successful(Status(statusCode)("Forbidden area"))
+      case 404 => Future.successful(Status(statusCode)("Page not found"))
+      case 407 => Future.successful(Status(statusCode)("Proxy Authentication Required"))
+      case 408 => Future.successful(Status(statusCode)("Request Time-Out"))
+      case 410 => Future.successful(Status(statusCode)("Gone"))
+      case 500 => Future.successful(Status(statusCode)("Internal Server Error"))
+      case 502 => Future.successful(Status(statusCode)("Bad Gateway"))
+      case 503 => Future.successful(Status(statusCode)("Service Temporarily Unavailable"))
+      case 504 => Future.successful(Status(statusCode)("Gateway Time-Out"))
+      case _=> Future.successful (Status (statusCode) ("Something went wrong!!"))
     }
   }
 
