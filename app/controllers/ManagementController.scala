22d0cf2456408eeda3633e46e636c2293b353013
Charmy Garg <charmiigarg@gmail.com>
2017-03-08 09:58:44 +0530

    Play2

--- app/controllers/ManagementController.scala
+++ app/controllers/ManagementController.scala
@@ -4,28 +4,26 @@ import com.google.inject.Inject
 import play.api.mvc.{Action, Controller}
 import services.ManagementService
 
-
 /**
   * Created by Charmy Garg on 08-Mar-17.
   */
 class ManagementController @Inject()(managementService: ManagementService) extends Controller{
 
   def management = Action { implicit request=>
-
     val list = managementService.getList
     Ok(views.html.management(list))
   }
 
   def enableUser(username:String) = Action{ implicit request=>
     managementService.enableUser(username)
-    Redirect(routes.LoginController.management()).flashing(
+    Redirect(routes.LoginController.login()).flashing(
       "success" -> "User enabled successfully")
   }
 
   def disableUser(username:String) = Action{ implicit request=>
     managementService.disableUser(username)
-    Redirect(routes.LoginController.management()).flashing(
-      "success" -> "User disabled successfully")
+    Redirect(routes.LoginController.login()).flashing(
+      "success" -> "User has been disabled")
   }
 
 }
